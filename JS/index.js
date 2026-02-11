8b56e871becccfd8acb4ac2910492bb5525fd54b
Santiago <dicolantonio@yahoo.com.ar>
2026-02-11 08:27:21 -0300

    tiempo de carga solucionado

--- JS/index.js
+++ JS/index.js
@@ -3,10 +3,8 @@ import { cargarProductos } from "./search/productStore.js";
 import { inicializarBusquedaGlobal } from "./search/searchInit.js";
 import { cargarComponentesComunes } from "./ui/loadCommonComponents.js";
 
+await cargarComponentesComunes();
 await cargarProductos();
-cargarComponentesComunes();
+inicializarBusquedaGlobal();
 
-setTimeout(() => {
-  inicializarBusquedaGlobal();
-}, 1500);
 
