7e319a5cf632be05e7be2d8e0d4c91c57bc48c87
Santiago <dicolantonio@yahoo.com.ar>
2026-02-18 09:55:29 -0300

    rutas

--- JS/ui/loadCommonComponents.js
+++ JS/ui/loadCommonComponents.js
@@ -1,19 +1,27 @@
+const isGitHubPages = location.hostname.includes("github.io");
+const BASE_PATH = isGitHubPages ? "/caz" : "";
+
 const load = async (id, file) => {
   const container = document.getElementById(id);
-  if (!container) return; // 👈 evita el error
+  if (!container) return;
 
-  const res = await fetch(file);
+  const res = await fetch(`${BASE_PATH}${file}`);
+  if (!res.ok) {
+    console.error("No se pudo cargar:", file);
+    return;
+  }
+
   container.innerHTML = await res.text();
 };
 
-
 export const cargarComponentesComunes = async () => {
   await Promise.all([
-    load("navBar", "./COMPONENTS/navBar.html"),
-    load("whatsApp", "./COMPONENTS/whatsApp.html"),
-    load("footer", "./COMPONENTS/footer.html"),
-    load("cards", "./COMPONENTS/cards.html"),
+    load("navBar", "/COMPONENTS/navBar.html"),
+    load("whatsApp", "/COMPONENTS/whatsApp.html"),
+    load("footer", "/COMPONENTS/footer.html"),
+    load("cards", "/COMPONENTS/cards.html"),
   ]);
 };
+
 
 
